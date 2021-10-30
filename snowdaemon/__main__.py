5cbadd76181a1af6a86a2128b58fa876c124b2b9
iaingray <igray@itgassociates.com>
2021-10-31 00:16:18 +0100

    copyright date update

--- snowdaemon/__main__.py
+++ snowdaemon/__main__.py
@@ -1,5 +1,5 @@
 #
-#  Copyright (c) 2020 Quantdeck Systems Ltd. All rights reserved.
+#  Copyright (c) 2021 Devdrop Ltd. All rights reserved.
 #
 #  This program is licensed to you under the Apache License Version 2.0, and
 #  you may not use this file except in compliance with the Apache License
